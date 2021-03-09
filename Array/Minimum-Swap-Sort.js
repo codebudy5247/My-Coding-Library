57bf4e502bc941a632a02dd1fec0b958dc69e9d8
codebudy5247 <a.shekhar5247@gmail.com>
2021-03-09 18:59:45 +0530

    sort array:bubble

---
+++ Array/Minimum-Swap-Sort.js
@@ -0,0 +1,18 @@
+function minimumSwaps(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != i + 1) {
+      let temp = arr[i];
+      arr[arr.indexOf(i + 1)] = temp;
+      arr[i] = i + 1;
+      count = count + 1;
+    }
+  }
+  return count;
+}
+console.log(minimumSwaps([7, 1, 2, 3, 4, 5, 8, 6]));
+console.log(minimumSwaps([33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]));
+
+
+
+
